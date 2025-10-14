df7f96ce466e5336245a553c5727fe283f470dd3
jenifa06 <157088205+jenifa06@users.noreply.github.com>
2025-10-14 22:49:10 +0530

    Update cakeshop.js

--- cakeshop.js
+++ cakeshop.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
-    function calculateTotal(){
+    $("#calc").click(function(){   // <-- make sure the button id matches 'calc'
         let rainbow = parseInt($("#rainbow").val()) || 0;
         let choco = parseInt($("#choco").val()) || 0;
         let velvet = parseInt($("#velvet").val()) || 0;
         let black = parseInt($("#black").val()) || 0;
 
         let total = (rainbow * 300) + (choco * 200) + (velvet * 250) + (black * 350);
-        $("#bill").html("Total Price: Rs. " + total);
-    }
 
-    // Trigger total update automatically on typing or changing input
-    $("input").on("keyup change", function(){
-        calculateTotal();
+        $("#bill").html("Total Price: Rs. " + total);
     });
 });
